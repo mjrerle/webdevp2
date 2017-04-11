eced75ad920ee8ea0e7cade3ec881e2ec5c6ce06
tjnolan24 <tjnolan@rams.colostate.edu>
2017-04-11 17:54:21 -0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -6,45 +6,60 @@ include "templates/jumbotron.php";
 $dbh = new Database();
 ?>
 
-<?php
-function view_cart(){
+<?php 
+function total (){
+	$total = 0.0;
 	$row = $_SESSION['array'];
+	
+	foreach ($row as $ing){
+		$total += $_SESSION['items'][$ing->id]['Total'];
+	}
+	
+	return $total;
+}
+?>
+
+<?php 
+function view_cart(){
+	$row = $_SESSION['array'];  
 	echo '<table class="table">';
-	echo '<tr>
-            <th>Name</th>
+	echo '<tr> 
+            <th>Name</th> 
             <th>Price</th>
 			<th>Quantity</th>
           </tr> ';
+
 	foreach ($row as $ing){
 		$price = number_format((float)$_SESSION['items'][$ing->id]['Total'], 2 , '.' , ''); // formatted to 2 decimals
 		$quant = $_SESSION['items'][$ing->id]['Quantity']; //quantity
-			echo '<tr>';
+			echo '<tr>'; 
 			echo "<td>$ing->name</td>";
 			echo "<td>$price</td>";
 			echo "<td>$quant</td>";
-			echo "<td><a href=\"cart.php?remove=yes&id=$ing->id\">Remove from Cart</a></td>";
+			echo "<td><a href=\"cart.php?remove=yes&id=$ing->id\">Remove from Cart</a></td>";			
 			echo '</tr>';
-
+		
 	}
 	echo '</table>';
 }
+
 function remove_item($id){
 	foreach($_SESSION['array'] as $itemsKey => $items){
 		foreach($items as $valueKey => $value){
 			if($valueKey == 'id' && $value == $id){
 				//delete this particular object from the $array
 				unset($_SESSION['array'][$itemsKey]);
-			}
+			} 
 		}
-	}
+	} 
 	if (empty($_SESSION['array']))
 		unset($_SESSION['array']);
 }
 ?>
 
-<?php
+<?php 
 if (isset($_GET['remove'])):
-	$id = strip_tags($_GET['id']);
+	$id = $_GET['id']; 
 	$quant = $_SESSION['items'][$id]['Quantity'];
 	if ($quant <= 1):
 		unset($_SESSION['items'][$id]); //remove
@@ -59,7 +74,7 @@ if (isset($_GET['remove'])):
 endif;
 ?>
 
-<?php
+<?php 
 if (!isset($_SESSION['array']) and !isset($_GET['id'])):
 	echo '<h3 align="center">Your Cart is Empty</h3>';
 	echo '<p align="center"><a href="products.php">Continue Shopping</a></p>';
@@ -70,21 +85,22 @@ endif;
 
 
 
-<?php
+<?php 
 if (isset($_GET['id'])):
-	$id = intval($_GET['id']);
+	$id = $_GET['id'];
 	if (!isset($_SESSION['array'])):
 		$row = array(); //stores ingredients in cart
 		$row[] = $dbh->getIngredientbyID($id);
 		$_SESSION['array'] = $row;
 		$_SESSION['items'] = array();
 		$_SESSION['items'][$id] = array('Quantity' => 1, 'Total' => $row[0]->price);
+
 	else:
-		$row = $_SESSION['array'];
+		$row = $_SESSION['array'];  
 		if (isset($_SESSION['items'][$id])): //item exists already
 			$_SESSION['items'][$id]['Quantity']++;
 			$price = $dbh->getIngredientbyID($id)->price;
-			$_SESSION['items'][$id]['Total'] += $price;
+			$_SESSION['items'][$id]['Total'] += $price; 
 		else:
 			//create a new item
 			$_SESSION['items'][$id] = array('Quantity' => 1, 'Total' => $dbh->getIngredientbyID($id)->price);
@@ -93,13 +109,17 @@ if (isset($_GET['id'])):
 		endif;
 	endif;
 endif;
+
+
 ?>
 
 <div class="cart">
 	<h2 align="center">My Cart</h2>
 	<?php view_cart(); ?>
+	<h3 align="center">Your Total is: $<?php echo number_format((float)total(), 2 , '.' , '');?></h3>
 	<p align="center"><a href="checkout.php">Checkout</a></p>
 	<p align="center"><a href="products.php">Continue Shopping</a></p>
 </div>
 
 <?php require 'templates/footer.php';?>
+
